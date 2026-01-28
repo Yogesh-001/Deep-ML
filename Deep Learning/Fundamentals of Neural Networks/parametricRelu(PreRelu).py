2ae8b4049743c9513dd010677a140737d7d5ae94
Yogesh-001 <74003370+Yogesh-001@users.noreply.github.com>
2026-01-28 11:05:39 +0530

    Create parametricRelu(PreRelu).py

---
+++ Deep Learning/Fundamentals of Neural Networks/parametricRelu(PreRelu).py
@@ -0,0 +1,16 @@
+def prelu(x: float, alpha: float = 0.25) -> float:
+	"""
+	Implements the PReLU (Parametric ReLU) activation function.
+
+	Args:
+		x: Input value
+		alpha: Slope parameter for negative values (default: 0.25)
+
+	Returns:
+		float: PReLU activation value
+	"""
+	# Your code here
+	if x > 0:
+		return x
+	else:
+		return alpha * x
